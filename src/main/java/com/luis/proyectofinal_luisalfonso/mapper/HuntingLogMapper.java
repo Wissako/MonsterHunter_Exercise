a5140d201f664d33bf4122cfcc2474241745fd39
wissako <luisalfonsoperezrojo@gmail.com>
2026-01-20 09:09:26 +0100

    Refactor HuntingLogMapper comments for clarity and remove unnecessary code

--- src/main/java/com/luis/proyectofinal_luisalfonso/mapper/HuntingLogMapper.java
+++ src/main/java/com/luis/proyectofinal_luisalfonso/mapper/HuntingLogMapper.java
@@ -1,7 +1,6 @@
 package com.luis.proyectofinal_luisalfonso.mapper;
 
 import com.luis.proyectofinal_luisalfonso.dto.response.HuntingLogResponse;
-import com.luis.proyectofinal_luisalfonso.dto.response.MonsterMapper;
 import com.luis.proyectofinal_luisalfonso.dto.response.MonsterResponse;
 import com.luis.proyectofinal_luisalfonso.models.entities.HuntingLog;
 import org.mapstruct.*;
@@ -11,10 +10,10 @@ public interface HuntingLogMapper {
 
     //Mapeo manual para sacar los nombres
 
-    // Del objeto 'hunter', coge el campo 'name' y ponlo en 'hunterName' del DTO
+    // Del objeto 'hunter', coge el campo 'name' y lo ponemos en 'hunterName' del DTO
     @Mapping(source = "hunter.name", target = "hunterName")
 
-    // Del objeto 'quest', coge el campo 'name' y ponlo en 'questName'
+    // Del objeto 'quest', coge el campo 'name' y lo ponemos  en 'questName'
     @Mapping(source = "quest.name", target = "questName")
 
     // Del objeto 'quest', entra en 'target' (Monstruo) y coge su 'name'
@@ -27,9 +26,4 @@ public interface HuntingLogMapper {
 
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     MonsterResponse partialUpdate(MonsterMapper monsterMapper, @MappingTarget MonsterResponse monsterResponse);
-
-    /* NOTA: No necesitamos 'toEntity' aquí porque en el Service creamos
-     la entidad manualmente para buscar los objetos Hunter y Quest por ID.
-     MapStruct por defecto no sabe convertir un "Long ID" a un objeto "Hunter".
-    */
 }
